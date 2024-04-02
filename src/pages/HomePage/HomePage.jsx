d0a19a2de3c3cebb4e349ac5c049a9357946df80
wdotgonzales <wdotgonzales@gmail.com>
2024-04-02 16:58:58 +0800

    import carousel component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,9 @@
 import NavBar from "../../NavBar/NavBar";
+import Carousel from "./Carousel/Carousel";
 const HomePage = () => {
     return <>
         <NavBar />
+        <Carousel />
     </>
 }
 
