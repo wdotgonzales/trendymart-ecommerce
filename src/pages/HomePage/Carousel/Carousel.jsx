6f4599121cf2b41b521e5b5344c7524212803448
wdotgonzales <wdotgonzales@gmail.com>
2024-04-03 11:48:37 +0800

    feat: carousel component done

--- src/pages/HomePage/Carousel/Carousel.jsx
+++ src/pages/HomePage/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './Carousel.css';
 import chair from './img/chair.png';
 import lamp from './img/lamp-removebg-preview.png';
@@ -25,9 +25,11 @@ const Carousel = () => {
     }, [currentSlide]);
 
     return (
-        <section className='bg-gray-200 mx-[7em] overflow-hidden'>
-            {/* Slides */}
-            <div className={`${currentSlide !== 1 ? 'hidden' : ''}`}>
+        <>
+            {/* xl screensize  */}
+            <section className='bg-gray-200 mx-[7em] overflow-hidden hidden lg:block'>
+                {/* Slides */}
+
                 <div className={`${currentSlide !== 1 ? 'hidden' : ''}`}>
                     <div className='flex justify-center'>
                         <div className='self-start ml-[5.5em] self-end mb-[5em]'>
@@ -39,57 +41,193 @@ const Carousel = () => {
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className={`${currentSlide !== 2 ? 'hidden' : ''}`}>
-                <div className='flex justify-center'>
-                    <div className='self-start ml-[5.5em] self-end mb-[5em]'>
-                        <p className='font-bold text-[30px] tracking-wider text-1'>PREMIUM COMFORT.</p>
-                        <p className='text-[20px] text-[#979797] w-[58%] text-2'>One-click import feature lets you import the complete Depot demo content with a single mouse click.</p>
-                    </div>
-                    <div className='flex'>
-                        <div>
-                            <img src={lamp} alt="" className='lamp' />
+
+                <div className={`${currentSlide !== 2 ? 'hidden' : ''}`}>
+                    <div className='flex justify-center'>
+                        <div className='self-start ml-[5.5em] self-end mb-[5em]'>
+                            <p className='font-bold text-[30px] tracking-wider text-1'>PREMIUM COMFORT.</p>
+                            <p className='text-[20px] text-[#979797] w-[58%] text-2'>One-click import feature lets you import the complete Depot demo content with a single mouse click.</p>
                         </div>
-                        <div>
-                            <img src={bulb} alt="" className='bulb' />
+                        <div className='flex'>
+                            <div>
+                                <img src={lamp} alt="" className='lamp' />
+                            </div>
+                            <div>
+                                <img src={bulb} alt="" className='bulb' />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className={`${currentSlide !== 3 ? 'hidden' : ''}`}>
-                <div className='flex justify-center'>
-                    <div className=' ml-[5.5em] self-end mb-[5em] mr-[9em]'>
-                        <p className='font-bold text-[30px] tracking-wider text-1'>CONTEMPORARY DESIGN.</p>
-                        <p className='text-[20px] text-[#979797] w-[58%] text-2'>A large set of beautiful & fully flexible homepage layouts lets you create your website quickly & easily.</p>
-                    </div>
-                    <div className='mr-7 border-2'>
-                        <img src={woodenChair} alt="" className='wooden-chair' />
+                <div className={`${currentSlide !== 3 ? 'hidden' : ''}`}>
+                    <div className='flex justify-center'>
+                        <div className=' ml-[5.5em] self-end mb-[5em] mr-[9em]'>
+                            <p className='font-bold text-[30px] tracking-wider text-1'>CONTEMPORARY DESIGN.</p>
+                            <p className='text-[20px] text-[#979797] w-[58%] text-2'>A large set of beautiful & fully flexible homepage layouts lets you create your website quickly & easily.</p>
+                        </div>
+                        <div className='mr-7 border-2'>
+                            <img src={woodenChair} alt="" className='wooden-chair' />
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            {/* Slide Number */}
-            <div className='absolute top-[20em] ml-[-3.5em]'>
-                <div className='absolute top-[1em] left-[3.5em] ml-[-3.5em]'>
-                    {[1, 2, 3].map((slideNumber) => (
+                {/* Slide Number */}
+                <div className='absolute top-[20em] ml-[-3.5em]'>
+                    <div className='absolute top-[1em] left-[3.5em] ml-[-3.5em]'>
+                        {[1, 2, 3].map((slideNumber) => (
+                            <div
+                                key={slideNumber}
+                                onClick={() => handleSlideChange(slideNumber)}
+                                onMouseEnter={() => setCurrentHoveredNumber(slideNumber)}
+                                onMouseLeave={() => setCurrentHoveredNumber(null)}
+                                className='mt-2'
+                            >
+                                <button className='flex items-center'>
+                                    <p className={`font-bold ${currentSlide === slideNumber || currentHoveredNumber === slideNumber ? 'hovered-black-text' : 'defaultTextColor'}`}>{slideNumber.toString().padStart(2, '0')}</p>
+                                    <div className={`p-[1px] w-[30px] ml-[10.5px] ${currentSlide === slideNumber || currentHoveredNumber === slideNumber ? 'extendLineWidthWithAnimationHover' : 'defaultLineBgColor'}`}></div>
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+            </section>
+
+            {/* lg screen size */}
+            <section className='bg-gray-200 overflow-hidden block lg:hidden hidden md:block'>
+                <div className={`${currentSlide !== 1 ? 'hidden' : ''}`}>
+                    <div className='flex justify-center'>
+                        <div className='self-start ml-[5.5em] self-end mb-[5em]'>
+                            <p className='font-bold text-[25px] tracking-wider text-1'>THINK DIFFERENT.</p>
+                            <p className='text-[17px] text-[#979797] w-[58%] text-2'>Depot is a unique & captivating theme designed specifically for all types of shops and online stores.</p>
+                        </div>
+                        <div className=''>
+                            <img src={chair} alt="" className='pb-5 chair' />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={`${currentSlide !== 2 ? 'hidden' : ''}`}>
+                    <div className='flex justify-center'>
+                        <div className='self-start ml-[5.5em] self-end mb-[5em]'>
+                            <p className='font-bold text-[25px] tracking-wider text-1'>PREMIUM COMFORT.</p>
+                            <p className='text-[17px] text-[#979797] w-[58%] text-2'>One-click import feature lets you import the complete Depot demo content with a single mouse click.</p>
+                        </div>
+                        <div className='flex'>
+                            <div>
+                                <img src={lamp} alt="" className='lamp lgSize' />
+                            </div>
+                            <div>
+                                <img src={bulb} alt="" className='bulb lgSize' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={`${currentSlide !== 3 ? 'hidden' : ''}`}>
+                    <div className='flex justify-center'>
+                        <div className=' ml-[5.5em] self-end mb-[5em] mr-[9em]'>
+                            <p className='font-bold text-[25px] tracking-wider text-1'>CONTEMPORARY DESIGN.</p>
+                            <p className='text-[17  px] text-[#979797] w-[58%] text-2'>A large set of beautiful & fully flexible homepage layouts lets you create your website quickly & easily.</p>
+                        </div>
+                        <div className='mr-7 border-2'>
+                            <img src={woodenChair} alt="" className='wooden-chair ' />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex justify-center gap-2 pb-3'>
+                    <button
+                        onClick={() => handleSlideChange(1)}>
                         <div
-                            key={slideNumber}
-                            onClick={() => handleSlideChange(slideNumber)}
-                            onMouseEnter={() => setCurrentHoveredNumber(slideNumber)}
-                            onMouseLeave={() => setCurrentHoveredNumber(null)}
-                            className='mt-2'
-                        >
-                            <button className='flex items-center'>
-                                <p className={`font-bold ${currentSlide === slideNumber || currentHoveredNumber === slideNumber ? 'hovered-black-text' : 'defaultTextColor'}`}>{slideNumber.toString().padStart(2, '0')}</p>
-                                <div className={`p-[1px] w-[30px] ml-[10.5px] ${currentSlide === slideNumber || currentHoveredNumber === slideNumber ? 'extendLineWidthWithAnimationHover' : 'defaultLineBgColor'}`}></div>
-                            </button>
+                            className={`${currentSlide === 1 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
                         </div>
-                    ))}
+                    </button>
+                    <button
+                        onClick={() => handleSlideChange(2)}>
+                        <div
+
+                            className={`${currentSlide === 2 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                        </div>
+                    </button>
+                    <button
+                        onClick={() => handleSlideChange(3)}>
+                        <div
+                            className={`${currentSlide === 3 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                        </div>
+                    </button>
                 </div>
-            </div>
-        </section>
+            </section >
+
+
+            {/* md screen size */}
+            < section className='bg-gray-200 overflow-hidden block md:hidden' >
+                <div className={`${currentSlide !== 1 ? 'hidden' : ''}`}>
+                    <div>
+                        <img src={chair} alt="" className='pb-5 chair max-w-[200px] m-auto' />
+                    </div>
+
+                    <div className='text-center'>
+                        <p className='font-bold text-[25px] tracking-wider text-1'>THINK DIFFERENT.</p>
+                        <p className='text-[17  px] text-[#979797] text-2 mx-5 mt-2 pb-5'>Depot is a unique & captivating theme designed specifically for all types of shops and online stores.</p>
+                    </div>
+                </div>
+
+                <div className={`${currentSlide !== 2 ? 'hidden' : ''}`}>
+                    <div className='flex justify-center'>
+                        <div className='h-[250px]'>
+                            <img src={lamp} alt="" className='lamp mdSize w-[200px]' />
+                        </div>
+                        <div>
+                            <img src={bulb} alt="" className='bulb mdSize w-[100px]' />
+                        </div>
+                    </div>
+
+                    <div className='text-center'>
+                        <p className='font-bold text-[25px] tracking-wider text-1'>PREMIUM COMFORT.</p>
+                        <p className='text-[17  px] text-[#979797] text-2 mx-5 mt-2 pb-5'>One-click import feature lets you import the complete Depot demo content with a single mouse click.</p>
+                    </div>
+                </div>
+
+                <div className={`${currentSlide !== 3 ? 'hidden' : ''}`}>
+                    <div>
+                        <img src={woodenChair} alt="" className='pb-5 chair max-w-[200px] m-auto' />
+                    </div>
+
+                    <div className='text-center'>
+                        <p className='font-bold text-[25px] tracking-wider text-1'>CONTEMPORARY DESIGN.</p>
+                        <p className='text-[17  px] text-[#979797] text-2 mx-5 mt-2 pb-5'>A large set of beautiful & fully flexible homepage layouts lets you create your website quickly & easily.</p>
+                    </div>
+                </div>
+
+
+                <div className='flex justify-center gap-2 pb-3'>
+                    <button
+                        onClick={() => handleSlideChange(1)}>
+                        <div
+                            className={`${currentSlide === 1 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                        </div>
+                    </button>
+                    <button
+                        onClick={() => handleSlideChange(2)}>
+                        <div
+
+                            className={`${currentSlide === 2 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                        </div>
+                    </button>
+                    <button
+                        onClick={() => handleSlideChange(3)}>
+                        <div
+                            className={`${currentSlide === 3 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                        </div>
+                    </button>
+                </div>
+            </section >
+
+
+        </>
+
     );
 };
 
