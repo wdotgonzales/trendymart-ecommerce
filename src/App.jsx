cbfc187c10474bb19c16de614f1039b0f6032682
wdotgonzales <wdotgonzales@gmail.com>
2024-04-04 16:33:34 +0800

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
 import HomePage from './pages/HomePage/HomePage';
 
-import { ChakraProvider } from '@chakra-ui/react';
-
 function App() {
   return (
     <>
-      <ChakraProvider>
-        <HomePage />
-      </ChakraProvider>
+      <HomePage />
     </>
   )
 }
