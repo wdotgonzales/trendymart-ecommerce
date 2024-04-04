9dc94ab834c1fbb5da0639c98132e562bb5f1cb5
wdotgonzales <wdotgonzales@gmail.com>
2024-04-04 09:31:29 +0800

    modify size of carousel circle navigation width and height

--- src/pages/HomePage/Carousel/Carousel.jsx
+++ src/pages/HomePage/Carousel/Carousel.jsx
@@ -141,20 +141,20 @@ const Carousel = () => {
                     <button
                         onClick={() => handleSlideChange(1)}>
                         <div
-                            className={`${currentSlide === 1 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                            className={`${currentSlide === 1 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[1em] w-[1em]`}>
                         </div>
                     </button>
                     <button
                         onClick={() => handleSlideChange(2)}>
                         <div
 
-                            className={`${currentSlide === 2 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                            className={`${currentSlide === 2 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[1em] w-[1em]`}>
                         </div>
                     </button>
                     <button
                         onClick={() => handleSlideChange(3)}>
                         <div
-                            className={`${currentSlide === 3 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                            className={`${currentSlide === 3 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[1em] w-[1em]`}>
                         </div>
                     </button>
                 </div>
@@ -206,20 +206,20 @@ const Carousel = () => {
                     <button
                         onClick={() => handleSlideChange(1)}>
                         <div
-                            className={`${currentSlide === 1 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                            className={`${currentSlide === 1 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[1em] w-[1em]`}>
                         </div>
                     </button>
                     <button
                         onClick={() => handleSlideChange(2)}>
                         <div
 
-                            className={`${currentSlide === 2 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                            className={`${currentSlide === 2 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[1em] w-[1em]`}>
                         </div>
                     </button>
                     <button
                         onClick={() => handleSlideChange(3)}>
                         <div
-                            className={`${currentSlide === 3 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[0.7em] w-[0.7em]`}>
+                            className={`${currentSlide === 3 ? 'bg-gray-500' : 'bg-gray-50'} w-[20px] rounded-full h-[1em] w-[1em]  `}>
                         </div>
                     </button>
                 </div>
