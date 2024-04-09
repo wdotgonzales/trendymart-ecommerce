f3d2599b6b1a01a2a0287f51c7f463814846305b
Wilson Gonzales <wdotgonzales@gmail.com>
2024-04-09 19:09:13 +0800

    footer done

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return <>
+        <footer className="bg-black h-[5em]">
+            <div className="pt-5">
+                <hr className="mx-5" style={{ backgroundColor: "red" }} />
+                <p className="text-[#979797] mt-5 ml-5">Created By : Wilson Gonzales ⓒ 2024</p>
+            </div>
+        </footer>
+    </>
+}
+
+export default Footer;
